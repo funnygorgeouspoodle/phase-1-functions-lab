790619a58897cf40b6ef00fb8388f8c3801a6b7a
Ashiq Legi <ashiqlegi@gmail.com>
2021-12-12 19:36:16 -0600

    complteted assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+    return Math.abs(x-42)
+    
+}
+
+function distanceFromHqInFeet(x) {
+    distanceFromHqInBlocks(x);
+    return Math.abs((x-42)*264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination-start)*264)
+}
+
+function calculatesFarePrice(start, destination) {
+    let y = distanceTravelledInFeet(start, destination)
+    if (y <=400) {
+        return 0
+    } 
+        else if (y >= 400 && y<= 2000) {
+            return parseFloat(((y*0.02)-8).toFixed(2))
+    }   else if (y >=2000 && y<=2500) {
+            return 25
+    }
+        else {
+            return 'cannot travel that far'
+    }
+}
